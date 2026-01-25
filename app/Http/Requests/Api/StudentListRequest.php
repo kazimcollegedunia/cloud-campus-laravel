52d3266be8ec1f7f5310d59f651a930765e76da5
Mohd Kazim <mohdkazim14@gmail.com>
2026-01-25 13:33:33 +0530

    abc

---
+++ app/Http/Requests/Api/StudentListRequest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+class StudentListRequest extends BaseQueryRequest
+{
+    protected function specificRules(): array
+    {
+        return [
+            'class_id'   => ['nullable', 'integer'],
+            'section_id' => ['nullable', 'integer'],
+            'status'     => ['nullable', 'in:active,inactive'],
+            'frequency'  => ['nullable', 'in:monthly,quarterly,yearly'],
+        ];
+    }
+}
